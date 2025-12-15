526369fbc0ab2db8129527f05af595d24a5e0a26
ASuGuit <adamstruzikguit@gmail.com>
2025-12-15 15:47:55 +0100

    simulations with outliers

--- codes/functions.R
+++ codes/functions.R
@@ -39,3 +39,23 @@ simulation_ztpois <- function(N, p_1, p_2) {
   
 }
 
+simulation_ztpois_out <- function(N, p_1, p_2) {
+  
+  values <- data.table(
+    value = rtpois(n = N, lambda = 10, a = 0)
+  )
+  values[, id := .I]
+  is_outlier <- runif(N) < 0.05
+  values[["value"]][is_outlier] <- rtpois(n = sum(is_outlier), lambda = 1000, a = 0) 
+
+  indices_1 <- rbinom(n = N, size = 1, prob = p_1)
+  indices_2 <- rbinom(n = N, size = 1, prob = p_2)
+  A <- values[as.logical(indices_1)]
+  B <- values[as.logical(indices_2)]
+  inter <- values[id %in% intersect(A[["id"]], B[["id"]])]
+  
+  est <- sum(A[["value"]]) * sum(B[["value"]]) / sum(inter[["value"]])
+  
+  list(est = est, sum = sum(values[["value"]]))
+  
+}
--- codes/simulation.R
+++ codes/simulation.R
@@ -64,3 +64,33 @@ for (N in N_s) {
   )
   
 }
+
+for (N in N_s) {
+  
+  results_final <- list()
+  
+  for (i in 1: 100) {
+    
+    results <- data.frame(p_1 = c(), p_2 = c(), est = c(), mean = c())
+    for (p in p_s) {
+      sim <- simulation_ztpois_out(N, p[1], p[2])
+      result <- sim$est
+      true_sum <- sim$sum
+      results <- rbind(results, c(p[1],  p[2], result, true_sum))
+    }
+    colnames(results) <- c("p_1", "p_2", "est", "sum")
+    results_final[[i]] <- results
+    
+  }
+  
+  ests <- Reduce(`+`, lapply(results_final, function(df) df[["est"]])) / 100
+  true_sums <- Reduce(`+`, lapply(results_final, function(df) df[["sum"]])) / 100
+  assign(paste0("table_", as.integer(N), "_ztpois_out"), 
+         data.table(
+           p_1 = results_final[[1]][["p_1"]],
+           p_2 = results_final[[1]][["p_2"]],
+           est_sum = ests,
+           true_sum = true_sums)
+  )
+  
+}
