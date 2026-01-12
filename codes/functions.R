bf72bdb5e13007a645d46090941a0e2b35f3213c
ASuGuit <adamstruzikguit@gmail.com>
2026-01-12 14:23:14 +0100

    simulations with imputed data

--- codes/functions.R
+++ codes/functions.R
@@ -59,3 +59,110 @@ simulation_ztpois_out <- function(N, p_1, p_2) {
   list(est = est, sum = sum(values[["value"]]))
   
 }
+
+simulation_w_imp <- function(N, p_1_s, p_2_s) {
+  
+  df <- data.table(X = sample(c("a", "b", "c"), size = N, replace = TRUE),
+                   Y = sample(c("e", "f", "g"), size = N, replace = TRUE))
+  params <- data.table(
+    X = rep(c("a", "b", "c"), each = 3),
+    Y = rep(c("e", "f", "g"), times = 3),
+    lambda_val = c(2.5, 3, 3.5, 4, 4.5, 5, 10, 50, 100)
+  )
+  df <- merge(df, params, by = c("X", "Y"), sort = FALSE)
+  df[, Z := rtpois(n = N, lambda = lambda_val, a = 0)]
+  df[, lambda_val := NULL]
+  
+  params_probs <- data.table(
+    X = rep(c("a", "b", "c"), each = 3),
+    Y = rep(c("e", "f", "g"), times = 3),
+    prob_1 = p_1_s, 
+    prob_2 = p_2_s
+  )
+  df <- merge(df, params_probs, by = c("X", "Y"), sort = FALSE)
+  df[, I1 := rbinom(n = .N, size = 1, prob = prob_1)]
+  df[, I2 := rbinom(n = .N, size = 1, prob = prob_2)]
+  df[, prob_1 := NULL]
+  df[, prob_2 := NULL]
+  
+  df[, Z_true := Z]
+  to_remove <- sample(1:N, size = floor(0.25 * N))
+  df[to_remove, Z := NA]
+  observed_means <- df[I1 == 1 | I2 == 1, .(mean_obs = mean(Z, na.rm = TRUE)), by = .(X, Y)]
+  df <- merge(df, observed_means, by = c("X", "Y"), all.x = TRUE, sort = FALSE)
+  df[is.na(Z) & (I1 == 1 | I2 == 1), Z := mean_obs]
+  df[, mean_obs := NULL]
+
+  true_total_Z <- sum(df[["Z_true"]])
+  
+  df_observed <- df[I1 == 1 | I2 == 1]
+  df_agg <- df_observed[, .(n_imp = sum(Z), n_true = sum(Z_true), count = .N), by = .(X, Y, I1, I2)]
+  df_agg[, I1 := as.factor(I1)]
+  df_agg[, I2 := as.factor(I2)]
+  df_hidden <- data.table(expand.grid(
+    X = c("a", "b", "c"),
+    Y = c("e", "f", "g"),
+    I1 = factor("0", levels = c("0", "1")),
+    I2 = factor("0", levels = c("0", "1"))
+  ))
+  
+  model_imp <- glm(n_imp ~ (I1 + I2) * (X + Y), 
+                   family = quasipoisson(), 
+                   data = df_agg)
+  model_true <- glm(n_true ~ (I1 + I2) * (X + Y), 
+                    family = quasipoisson(), 
+                    data = df_agg)
+  model_count <- glm(count ~ (I1 + I2) * (X + Y),
+                     family = quasipoisson(),
+                     data = df_agg)
+  
+  df_hidden[, Z_pred_imp := predict(model_imp, newdata = df_hidden, type = "response")]
+  df_hidden[, Z_pred_true := predict(model_true, newdata = df_hidden, type = "response")]
+  df_hidden[, n_pred := predict(model_count, newdata = df_hidden, type = "response")]
+  
+  df_hidden <- merge(df_hidden, observed_means, by = c("X", "Y"), all.x = TRUE, sort = FALSE)
+  df_hidden[, Z_pred_from_count := n_pred * mean_obs]
+  
+  est_total_Z_imp <- sum(df_observed[["Z"]]) + sum(df_hidden[["Z_pred_imp"]])
+  est_total_Z_true <- sum(df_observed[["Z_true"]]) + sum(df_hidden[["Z_pred_true"]])
+  est_total_Z_from_count <- sum(df_observed[["Z"]]) + sum(df_hidden[["Z_pred_from_count"]])
+  
+  data.table(true_total_Z = true_total_Z,
+             est_total_Z_true = est_total_Z_true,
+             est_total_Z_imp = est_total_Z_imp,
+             est_total_Z_from_count = est_total_Z_from_count)
+  
+}
+
+calculate_metrics <- function(df_results) {
+  
+  metrics_list <- list()
+  
+  for (est in c("est_total_Z_true", "est_total_Z_imp", "est_total_Z_from_count")) {
+    error <- df_results[[est]] - df_results[["true_total_Z"]]
+    
+    bias <- mean(error)
+    rel_bias <- mean(error / df_results[["true_total_Z"]])
+    rmse <- sqrt(mean(error^2))
+    mean_value <- mean(df_results[[est]])
+    
+    metrics_list[[est]] <- data.table(
+      method = est,
+      mean_value = mean_value,
+      mean_bias = bias,
+      mean_rel_bias = rel_bias * 100,
+      rmse = rmse
+    )
+  }
+  
+  metrics_list[["true_total_Z"]] <- data.table(
+    method = "true_total_z",
+    mean_value = mean(df_results[["true_total_Z"]]),
+    mean_bias = 0,
+    mean_rel_bias = 0,
+    rmse = 0
+  )
+  
+  rbindlist(metrics_list)
+  
+}
---
+++ codes/sim_w_imp.R
@@ -0,0 +1,33 @@
+source("codes/functions.R")
+set.seed(123)
+
+p_1_s <- c(0.3, 0.4, 0.5, 0.4, 0.3, 0.4, 0.5, 0.4, 0.3)
+p_2_s <- c(0.4, 0.3, 0.4, 0.5, 0.4, 0.3, 0.4, 0.5, 0.4)
+
+results_1 <- data.table(true_total_Z = numeric(),
+                        est_total_Z_true = numeric(),
+                        est_total_Z_imp = numeric(),
+                        est_total_Z_from_count = numeric())
+
+for (i in 1:1000) {
+  res <- simulation_w_imp(10000, p_1_s, p_2_s)
+  results_1 <- rbind(results_1, res)
+}
+
+result_imp_1 <- calculate_metrics(results_1)
+
+p_1_s <- p_1_s - 0.2
+p_2_s <- p_2_s - 0.2
+
+results_2 <- data.table(true_total_Z = numeric(),
+                        est_total_Z_true = numeric(),
+                        est_total_Z_imp = numeric(),
+                        est_total_Z_from_count = numeric())
+
+for (i in 1:1000) {
+  res <- simulation_w_imp(10000, p_1_s, p_2_s)
+  results_2 <- rbind(results_2, res)
+}
+
+result_imp_2 <- calculate_metrics(results_2)
+
